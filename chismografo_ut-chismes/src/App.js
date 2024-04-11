e6e58edc87cc37618bb810a1106e9230b095b434
AndiV18 <a1122150020@utch.edu.mx>
2024-04-11 12:15:12 -0600

    final flash
    haaa

--- chismografo_ut-chismes/src/App.js
+++ chismografo_ut-chismes/src/App.js
@@ -3,26 +3,10 @@ import './App.css';
 import Header from './componentes/Header';
 import Footer from './componentes/Footer';
 import Menu from './componentes/Menu';
-import Mecatrónica from './componentes/meca';
-import Ti from './components/ti';
-import DN from './componentes/DN';
+
 
 function App() {
-  const [selectedComponent, setSelectedComponent] = useState(null);
-
-  const renderComponent = () => {
-    switch (selectedComponent) {
-      case 'meca':
-        return <Mecatrónica />;
-      case 'ti':
-        return <Ti />;
-      case 'dn':
-        return <DN />;
-      default:
-        return null;
-    }
-  };
-
+  
   return (
     <div className="App">
 
@@ -30,8 +14,8 @@ function App() {
       <Menu/>
       
       <Footer/>
-
-     
+      
+      
     </div>
   );
 }
--- chismografo_ut-chismes/src/componentes/Menu.jsx
+++ chismografo_ut-chismes/src/componentes/Menu.jsx
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Menu.css'; // Asegúrate de tener un archivo de estilos CSS para tus estilos personalizados
+import Mecatrónica from './Meca';
+import DN from './DN';
+import Ti from './Ti';
 
 function Menu() {
+  const [selectedComponent, setSelectedComponent] = useState('');
+  const renderComponent = () => {
+    switch (selectedComponent) {
+      case 'Meca':
+        return <Mecatrónica />;
+      case 'Ti':
+        return <Ti />;
+      case 'dn':
+        return <DN />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <div className="App">
-      
       <div className="button-container">
-      <button class="big-button button1">TI</button>
-      <button class="big-button button2">Mecatronica</button>
-      <button class="big-button button3">DN</button>
-
+        <button className="big-button button1" onClick={() => setSelectedComponent('Meca')}>Mecatrónica</button>
+        <button className="big-button button2" onClick={() => setSelectedComponent('Ti')}>TI</button>
+        <button className="big-button button3" onClick={() => setSelectedComponent('dn')}>DN</button>
+      </div>
+      <div className="component-container">
+        {renderComponent()}
       </div>
     </div>
   );
